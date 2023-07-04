bd3cd86b844641db089a676df1d468f10a49c340
bifnudozhao <bifnudozhao@tencent.com>
2023-07-04 11:22:36 +0800

    mod: refactor scenes

---
+++ src/ray-tracer/demo-scenes/many-balls.ts
@@ -0,0 +1,79 @@
+import { Vector3 } from '@TRE/math'
+import { Camera } from '@TRE/ray-tracer/camera'
+import { Color } from '@TRE/ray-tracer/color'
+import { Sphere } from '@TRE/ray-tracer/primitives'
+import { Hittables } from '@TRE/ray-tracer/hittables'
+import {
+  DielectricMaterial,
+  LambertianMaterial,
+  MetalMaterial,
+} from '@TRE/ray-tracer/materials'
+
+export const manyBallsScene = (aspectRatio: number) => {
+  const world = new Hittables()
+
+  const groundMaterial = new LambertianMaterial({
+    albedo: new Color(0.5, 0.5, 0.5),
+  })
+  const ground = new Sphere(new Vector3(0, -1000, 0), 1000)
+  ground.setMaterial(groundMaterial)
+  world.add(ground)
+
+  const gridSize = 10
+  const step = 3
+  for (let a = -gridSize; a < gridSize; a += step) {
+    for (let b = -gridSize; b < gridSize; b += step) {
+      const random = Math.random()
+      const center = new Vector3(
+        a + 0.9 * Math.random() * step,
+        0.2,
+        b + 0.9 * Math.random() * step
+      )
+
+      const ball = new Sphere(center, 0.2)
+      let material
+      if (center.sub(new Vector3(4, 0.2, 0)).len() > 0.9) {
+        if (random < 0.8) {
+          // diffuse
+          material = new LambertianMaterial({
+            albedo: Color.random(),
+          })
+        } else if (random < 0.95) {
+          // metal
+          material = new MetalMaterial({
+            albedo: Color.random(),
+            fuzz: Math.random() * 0.5,
+          })
+        } else {
+          // glass
+          material = new DielectricMaterial({ refractiveIndex: 1.5 })
+        }
+
+        ball.setMaterial(material)
+        world.add(ball)
+      }
+    }
+  }
+
+  // three big balls
+  const m1 = new DielectricMaterial({ refractiveIndex: 1.5 })
+  const b1 = new Sphere(new Vector3(0, 1, 0), 1.0)
+  b1.setMaterial(m1)
+
+  const m2 = new LambertianMaterial({ albedo: new Color(0.4, 0.2, 0.1) })
+  const b2 = new Sphere(new Vector3(-4, 1, 0), 1.0)
+  b2.setMaterial(m2)
+
+  const m3 = new MetalMaterial({ albedo: new Color(0.7, 0.6, 0.5), fuzz: 0 })
+  const b3 = new Sphere(new Vector3(4, 1, 0), 1.0)
+  b3.setMaterial(m3)
+
+  world.add(b1)
+  world.add(b2)
+  world.add(b3)
+
+  const camera = new Camera({ aspectRatio: 3 / 2, fov: 20 })
+  camera.look(new Vector3(13, 2, 3), new Vector3(0, 0, 0), new Vector3(0, 1, 0))
+
+  return { camera, world }
+}
---
+++ src/ray-tracer/demo-scenes/three-balls.ts
@@ -0,0 +1,81 @@
+import { Vector3 } from '@TRE/math'
+import { Camera } from '@TRE/ray-tracer/camera'
+import { Color } from '@TRE/ray-tracer/color'
+import { Sphere, XYRect } from '@TRE/ray-tracer/primitives'
+import { Hittables } from '@TRE/ray-tracer/hittables'
+import {
+  DielectricMaterial,
+  LambertianMaterial,
+  MetalMaterial,
+} from '@TRE/ray-tracer/materials'
+import { SolidColor, CheckerTexture } from '@TRE/ray-tracer/texture'
+import { DiffuseLight } from '@TRE/ray-tracer/materials'
+
+export const threeBallsScene = (aspectRatio: number) => {
+  const camera = new Camera({
+    fov: 40,
+    aspectRatio,
+  })
+  camera.look(
+    new Vector3(-3, 2, 0),
+    new Vector3(0, 0, -1),
+    new Vector3(0, 1, 0)
+  )
+
+  const checkerTexture1 = new CheckerTexture(
+    new SolidColor(new Color(0.2, 0.3, 0.1)),
+    new SolidColor(new Color(0.9, 0.9, 0.9))
+  )
+  const checkerTexture2 = new CheckerTexture(
+    new SolidColor(new Color(0.1, 0.2, 0.5)),
+    new SolidColor(new Color(0.9, 0.9, 0.9)),
+    50
+  )
+
+  const groundMaterial = new LambertianMaterial({
+    albedo: new Color(0.8, 0.8, 0.8),
+  })
+  groundMaterial.setTexture(checkerTexture1)
+  const centerBallMaterial = new LambertianMaterial({
+    albedo: new Color(0.1, 0.2, 0.5),
+  })
+  centerBallMaterial.setTexture(checkerTexture2)
+  const metalMaterial1 = new MetalMaterial({
+    albedo: new Color(0.8, 0.6, 0.2),
+    fuzz: 0,
+  })
+  const metalMaterial2 = new MetalMaterial({
+    albedo: new Color(0.8, 0.6, 0.2),
+    fuzz: 1,
+  })
+  const dielectricMaterial = new DielectricMaterial({
+    refractiveIndex: 1.5,
+  })
+  const diffuseLight = new DiffuseLight()
+  diffuseLight.setTexture(new SolidColor(new Color(4, 4, 4)))
+
+  const ground = new Sphere(new Vector3(0, -100.5, -1), 100)
+  const centerBall = new Sphere(new Vector3(0, 0, -1), 0.5)
+  const leftBallInner = new Sphere(new Vector3(-1, 0, -1), -0.4)
+  const leftBallOuter = new Sphere(new Vector3(-1, 0, -1), 0.5)
+  const rightBall = new Sphere(new Vector3(1, 0, -1), 0.5)
+  const rectLight = new XYRect(-1, 1, 0, 1, -2)
+
+  ground.setMaterial(groundMaterial)
+  centerBall.setMaterial(centerBallMaterial)
+  leftBallInner.setMaterial(dielectricMaterial)
+  leftBallOuter.setMaterial(dielectricMaterial)
+  rightBall.setMaterial(metalMaterial1)
+  rectLight.setMaterial(diffuseLight)
+
+  const world = new Hittables()
+
+  world.add(ground)
+  world.add(centerBall)
+  world.add(leftBallInner)
+  world.add(leftBallOuter)
+  world.add(rightBall)
+  world.add(rectLight)
+
+  return { camera, world }
+}
